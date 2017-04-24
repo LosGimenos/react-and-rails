52c4c8b8163695970b1e8cc6ec8a1b7298976e08
Larry Hernandez <larryhernanjr@gmail.com>
2017-04-24 10:30:26 -0400

    add api logic for components

--- item_cart/app/assets/javascripts/components/_all_items.js.jsx
+++ item_cart/app/assets/javascripts/components/_all_items.js.jsx
@@ -1,6 +1,13 @@
 var AllItems = React.createClass({
+  getInitialState() {
+    return { items: [] }
+  },
+
   componentDidMount() {
-    console.log('Component mounted');
+    $.getJSON('/api/v1/items.json', response => {
+      this.setState({ items: response }
+      )}
+    );
   },
 
   render() {
